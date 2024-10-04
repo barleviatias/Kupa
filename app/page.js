8909e6efe6a439a3eb1842beed2ba2b9bbf87e8e
barleviatias <barlevi.atias@gmail.com>
2024-10-05 01:06:01 +0300

    add support of no results

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
 	const [resultCount, setResultCount] = useState('');
 	const [videosData, setVideosData] = useState([]);
 	const [searchResults, setSearchResults] = useState([]);
-  const [expandedContext, setExpandedContext] = useState(null);
+	const [expandedContext, setExpandedContext] = useState(null);
 
 	const fetchData = async () => {
 		const response = await fetch('/api/counter', {
@@ -22,37 +22,36 @@ export default function Home() {
 		console.log('run');
 		console.log(data.documentCount);
 	};
+
 	useEffect(() => {
 		fetchData();
-	}, []); // Empty dependency array
-	function convertToList(objectOfObjects) {
-		// Convert the object to an array of values
-		const arrayOfObjects = Object.values(objectOfObjects);
+	}, []);
 
-		// Sort the array based on context length (descending order)
+	function convertToList(objectOfObjects) {
+		const arrayOfObjects = Object.values(objectOfObjects);
 		const sortedArray = arrayOfObjects.sort((a, b) => {
-      const lengthA = a.context ? a.context.length : 0;
-      const lengthB = b.context ? b.context.length : 0;
-      return lengthB - lengthA;
-    });
-    
-    // Map the sorted array to the desired format
-    return sortedArray.map(item => ({
-      url: item.url || '',
-      context: item.context || '',
-      contextCount: item.context ? item.context.length : 0,
-      episode_name: item.episode_name || '',
-      episode_number: item.episode_number || '',
-      season_number: item.season_number || '',
-    }));
+			const lengthA = a.context ? a.context.length : 0;
+			const lengthB = b.context ? b.context.length : 0;
+			return lengthB - lengthA;
+		});
+		return sortedArray.map((item) => ({
+			url: item.url || '',
+			context: item.context || [],
+			contextCount: item.context ? item.context.length : 0,
+			episode_name: item.episode_name || '',
+			episode_number: item.episode_number || '',
+			season_number: item.season_number || '',
+		}));
 	}
-  const toggleContext = (index) => {
-    if (expandedContext === index) {
-      setExpandedContext(null);
-    } else {
-      setExpandedContext(index);
-    }
-  };
+
+	const toggleContext = (index) => {
+		if (expandedContext === index) {
+			setExpandedContext(null);
+		} else {
+			setExpandedContext(index);
+		}
+	};
+
 	const handleSubmit = async (event) => {
 		setLoading(true);
 		event.preventDefault();
@@ -64,30 +63,26 @@ export default function Home() {
 			try {
 				const data = await response.json();
 				console.log(data);
-
 				setSearchResults(Object.keys(data).length);
-				// Set the count of results
 				let temp = convertToList(data);
-        console.log(`this is the temp ${temp}`);
-        
-				setVideosData(temp); // Convert and sort the data
+				console.log(`this is the temp ${temp}`);
+				setVideosData(temp.length > 0 ? temp : []);
 				setLoading(false);
 			} catch (error) {
 				console.error('Error parsing JSON:', error);
+				setVideosData([]);
 				setLoading(false);
-				// Handle the error appropriately (e.g., show an error message to the user)
 			}
 		} else {
 			console.error('Request failed with status:', response.status);
+			setVideosData([]);
 			setLoading(false);
-			// Handle the error appropriately (e.g., show an error message to the user)
 		}
 	};
+
 	return (
 		<div className="min-h-screen bg-gray-100 rtl">
-			<main
-				className="container mx-auto py-10 min-h-[90vh] rtl px-3 sm:px-2
-      ">
+			<main className="container mx-auto py-10 min-h-[90vh] rtl px-3 sm:px-2">
 				<div className="flex flex-col items-center">
 					<Image
 						src={Logo}
@@ -114,7 +109,7 @@ export default function Home() {
 						/>
 						<button
 							type="submit"
-							className="px-4 py-2 ml-2 bg-custom-red text-white rounded-lg hover:bg-blue-600">
+							className="px-4 py-2 ml-2 bg-custom-red text-white rounded-lg hover:bg-hover-red">
 							חפש לי
 						</button>
 					</form>
@@ -127,43 +122,65 @@ export default function Home() {
 				</div>
 				{loading ? (
 					<Loader />
+				) : videosData.length === 0 ? (
+					<div className="text-center mt-8">
+						<p className="text-xl font-bold text-custom-red">שומו שמיים אין תוצאות! אל חשש בואו ננסה שוב!</p>
+						<p className="mt-2 text-gray-600">נסה לחפש משהו אחר או לשנות את מילות החיפוש שלך.</p>
+					</div>
 				) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {videosData.map((item, index) => (
-            <div key={index} className="video-wrapper bg-white p-4 rounded-lg shadow">
-              <div className="relative pt-[56.25%]">
-                <iframe
-                  src={`https://www.youtube.com/embed/${item.url.split('watch?v=')[1]}`}
-                  allowFullScreen
-                  className="absolute top-0 left-0 w-full h-full"
-                ></iframe>
-              </div>
-              <p className="mt-2 text-black text-sm italic">{item.context[0]}</p>
-              <p className="mt-2 text-black font-bold">מספר הקשרים: {item.contextCount}</p>
-              <button
-                onClick={() => toggleContext(index)}
-                className="mt-2 px-4 py-2 bg-custom-red text-white rounded-lg hover:bg-blue-600"
-              >
-                {expandedContext === index ? 'הסתר הקשרים' : 'הצג הקשרים'}
-              </button>
-              {expandedContext === index && (
-                <div className="mt-4">
-                  <h3 className="font-bold mb-2">כל ההקשרים:</h3>
-                  <ul className="list-disc text-black list-inside">
-                    {item.context.map((context, idx) => (
-                      <li key={idx} className="mb-2">{context}</li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
-    </main>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+						{videosData.map((item, index) => (
+							<div
+								key={index}
+								className="video-wrapper bg-white p-4 rounded-lg shadow">
+								<div className="relative pt-[56.25%]">
+									<iframe
+										src={`https://www.youtube.com/embed/${
+											item.url.split('watch?v=')[1]
+										}`}
+										allowFullScreen
+										className="absolute top-0 left-0 w-full h-full"></iframe>
+								</div>
+								{item.context && item.context.length > 0 && (
+									<>
+										<p className="mt-2 text-black font-bold">
+											מספר אזכורים: {item.contextCount}
+										</p>
+										<p className="mt-2 text-black text-sm italic">
+											{item.context[0]}
+										</p>
+										
+										{item.context.length > 1 && (
+											<>
+												<button
+													onClick={() => toggleContext(index)}
+													className="mt-2 px-4 py-2 bg-custom-red text-white rounded-lg hover:bg-hover-red">
+													{expandedContext === index ? 'הסתר אזכורים' : 'הצג עוד אזכורים'}
+												</button>
+												{expandedContext === index && (
+													<div className="mt-4">
+														<h3 className="font-bold mb-2">אזכורים נוספים:</h3>
+														<ul className="list-disc text-black list-inside">
+															{item.context.slice(1).map((context, idx) => (
+																<li key={idx} className="mb-2">
+																	{context}
+																</li>
+															))}
+														</ul>
+													</div>
+												)}
+											</>
+										)}
+									</>
+								)}
+							</div>
+						))}
+					</div>
+				)}
+			</main>
 
-			<footer className="bg-gray-800 text-white flex  items-center py-4 bottom-0  min-h-[10vh]">
-				<div className="container mx-auto flex justify-center  text-center">
+			<footer className="bg-gray-800 text-white flex items-center py-4 bottom-0 min-h-[10vh]">
+				<div className="container mx-auto flex justify-center text-center">
 					<div className="flex justify-center items-center mb-2">
 						<a
 							href="https://www.linkedin.com/in/bar-levi-atias-/"
